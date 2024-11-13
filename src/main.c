96cd793f7120855bc1b3d862e74ab40da350b20b
alexexisted <a.assjones@list.ru>
2024-11-13 12:40:13 +0100

    created user name check(not working)

--- src/main.c
+++ src/main.c
@@ -1,6 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+#define MAX_NAME_LENGTH 20
+
+char globalPlayer1[20];
+char globalPlayer2[20];
+
+bool isNameValid(char name) {
+    const size_t nameLen = strlen(name);
+    switch (nameLen) {
+        case 0:
+            return false;
+            break;
+
+        default:
+            return true;
+            break;
+    }
+}
+
+void getPlayer2() {
+    printf("Enter the name of the Player2\n");
+    scanf("%s", &globalPlayer2);
+    if (isNameValid(globalPlayer2) == true) {
+        printf("Nice name");
+    } else {
+        printf("Incorrect name, try again");
+        getPlayer2();
+    }
+}
+
+void getPlayer1() {
+    printf("Enter the name of the Player1\n");
+    scanf("%s", &globalPlayer1);
+    if (isNameValid(globalPlayer1) == true) {
+        getPlayer2();
+    } else {
+        printf("Incorrect name, try again");
+        getPlayer1();
+    }
+}
 
 int mainMenu() {
     printf("1 - Play new game\n2 - Load saved game\n3 - Exit the game\n");
@@ -14,6 +54,7 @@ int mainMenu() {
             break;
         case 1:
             printf("TODO Starting new game...\n");
+            getPlayer1();
             break;
         case 2:
             printf("TODO saved games menu");
@@ -21,7 +62,6 @@ int mainMenu() {
         case 3:
             printf("See you again!");
             return EXIT_SUCCESS;
-            break;
         default:
             printf("Some error occurred");
             break;

8d02d1401579f03170647fd86692214745930e4c
alexexisted <a.assjones@list.ru>
2024-11-13 10:00:52 +0100

    init commit + main menu

---
+++ src/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+
+int mainMenu() {
+    printf("1 - Play new game\n2 - Load saved game\n3 - Exit the game\n");
+    int mainOption;
+    scanf("%d", &mainOption);
+
+    switch (mainOption) {
+        case 0:
+            printf("Enter valid value 1-3\n");
+            mainMenu();
+            break;
+        case 1:
+            printf("TODO Starting new game...\n");
+            break;
+        case 2:
+            printf("TODO saved games menu");
+            break;
+        case 3:
+            printf("TODO Exiting");
+            break;
+        default:
+            printf("Some error occurred");
+            break;
+    }
+
+    return 0;
+}
+
+int main(void) {
+    mainMenu();
+    return 0;
+}
